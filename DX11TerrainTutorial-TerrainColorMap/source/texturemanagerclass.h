d622c99d0fb5bf89264fd11d8e53c20786e42680
KsGin <imqqyangfan@gmail.com>
2018-05-06 10:24:58 +0800

    Update : 2018.5.6 by ksgin

---
+++ DX11TerrainTutorial-TerrainColorMap/source/texturemanagerclass.h
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: texturemanagerclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _TEXTUREMANAGERCLASS_H_
+#define _TEXTUREMANAGERCLASS_H_
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "textureclass.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: TextureManagerClass
+////////////////////////////////////////////////////////////////////////////////
+class TextureManagerClass
+{
+public:
+	TextureManagerClass();
+	TextureManagerClass(const TextureManagerClass&);
+	~TextureManagerClass();
+
+	bool Initialize(int);
+	void Shutdown();
+
+	bool LoadTexture(ID3D11Device*, ID3D11DeviceContext*, char*, int);
+
+	ID3D11ShaderResourceView* GetTexture(int);
+
+private:
+	TextureClass* m_TextureArray;
+	int m_textureCount;
+};
+
+#endif
